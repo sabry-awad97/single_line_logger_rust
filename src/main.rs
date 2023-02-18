797b9094ecd95e62c6e38238fb46152fdca89c82
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-18 13:08:02 +0200

    implement LogLevel and LogMessage

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 use std::io::{self, Write};
+
+// Enum defining the possible log levels
+enum LogLevel {
+    Info,
+    Warning,
+    Error,
+}
+
+// Struct defining a log message
+struct LogMessage {
+    level: LogLevel,
+    message: String,
+}
+
+// Struct implementing a single-line logger
 struct SingleLineLogger<W: Write> {
     stdout: W,
 }
 
 impl<W: Write> SingleLineLogger<W> {
+    // Method to create a new instance of SingleLineLogger
     fn new(stdout: W) -> Self {
         SingleLineLogger { stdout }
     }
 
-    fn log(&mut self, msg: &str) -> io::Result<()> {
+    // Method to log a message at the specified level
+    fn log(&mut self, level: LogLevel, message: &str) -> io::Result<()> {
         // Move the cursor up one line and clear the line
         write!(self.stdout, "\x1B[1F\x1B[0J")?;
 
-        // Write new output
-        write!(self.stdout, "{}", msg)?;
+        // Format the log message
+        let log_message = format!("[{}] {}", level_string(level), message);
+
+        // Write the log message to the console
+        write!(self.stdout, "{}", log_message)?;
 
         // Flush the output immediately
         self.stdout.flush()?;
@@ -22,9 +42,24 @@ impl<W: Write> SingleLineLogger<W> {
     }
 }
 
+// Helper function to convert a LogLevel to a string
+fn level_string(level: LogLevel) -> &'static str {
+    match level {
+        LogLevel::Info => "INFO",
+        LogLevel::Warning => "WARNING",
+        LogLevel::Error => "ERROR",
+    }
+}
+
 fn main() -> io::Result<()> {
+    // Create a new SingleLineLogger instance with stdout as the output
     let mut logger = SingleLineLogger::new(io::stdout());
-    logger.log("Sabry\n")?;
-    logger.log("Awad")?;
+
+    // Log some messages at different log levels
+    logger.log(LogLevel::Info, "Starting program...")?;
+    logger.log(LogLevel::Warning, "Something unusual happened.")?;
+    logger.log(LogLevel::Error, "An error occurred.")?;
+
+    // Signal that the program has completed successfully
     Ok(())
 }
